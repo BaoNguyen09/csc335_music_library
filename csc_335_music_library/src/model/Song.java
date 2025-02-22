af0ea7151733833a619c6e62e30fb1295280a2da
BaoNguyen09 <nttbvlogs@gmail.com>
2025-02-22 10:34:36 -0700

    feat: add instance variable and constructor method for Song class

--- csc_335_music_library/src/model/Song.java
+++ csc_335_music_library/src/model/Song.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Song {
-
+	private String songTitle;
+	private String artist;
+	private String albumTitle;
+	public enum Rating {
+		UNRATED,
+		ONE_STAR,
+		TWO_STAR,
+		THREE_STAR,
+		FOUR_STAR,
+		FIVE_STAR
+	};
+	private Rating rating;
+	
+	public Song(String songTitle, String artist, String albumTitle) {
+		this.songTitle = songTitle;
+		this.artist = artist;
+		this.albumTitle = albumTitle;
+	}
 }

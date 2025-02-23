15e664bf77cc87d3f217fd33d32f466ca87d8b5c
BaoNguyen09 <nttbvlogs@gmail.com>
2025-02-23 11:17:22 -0700

    feat: add a copy constructor and DbC for Song class

--- csc_335_music_library/src/model/Song.java
+++ csc_335_music_library/src/model/Song.java
@@ -15,12 +15,22 @@ public class Song {
 	private Rating rating;
 	private boolean isFavorite;
 	
+	/* @pre songTitle != null & artist != null && albumTitle != null */
 	public Song(String songTitle, String artist, String albumTitle) {
 		this.songTitle = songTitle;
 		this.artist = artist;
 		this.albumTitle = albumTitle;
 		rating = Rating.UNRATED;
 		isFavorite = false;
+	}
+	
+	/* Copy constructor */
+	public Song(Song anotherSong) {
+		this.songTitle = anotherSong.getSongTitle();
+		this.artist = anotherSong.getArtist();
+		this.albumTitle = anotherSong.getAlbumTitle();
+		rating = anotherSong.getRating();
+		isFavorite = anotherSong.isFavorite();
 	}
 	
 	public String getSongTitle() {

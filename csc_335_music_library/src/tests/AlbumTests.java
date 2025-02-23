2a37502fcbd3a6aba1ec88f47a13dddf4621d97e
BaoNguyen09 <nttbvlogs@gmail.com>
2025-02-23 12:22:55 -0700

    test: add test for copy constructor and getSongArray of Album class

--- csc_335_music_library/src/tests/AlbumTests.java
+++ csc_335_music_library/src/tests/AlbumTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 import model.Album;
@@ -52,6 +54,40 @@ class AlbumTests {
 		expectedSongList[2] = expectedSong3;
 		
 		assertArrayEquals(expectedSongList, album.getSongs(), "getSongs should return a list of song in format: title, artist, album");		
+	}
+	
+	@Test
+	void testGetSongArray() {
+		Song song1 = new Song("Daydreamer", "Adele", "19");
+		Song song2 = new Song("Best for Last", "Adele", "19");
+		Song song3 = new Song("Tired", "Adele", "19");
+		Album album = new Album("19", "Adele", "Pop", "2008");
+		album.addSong(song1);
+		album.addSong(song2);
+		album.addSong(song3);
+
+		ArrayList<Song> expectedSongList = new ArrayList<Song>();
+		expectedSongList.add(song1);
+		expectedSongList.add(song2);
+		expectedSongList.add(song3);
+		
+		assertEquals(expectedSongList, album.getSongArray(), "getSongs should return a list of song in format: title, artist, album");		
+	}
+	
+	@Test
+	void testCopyConstructor() {
+		Album album = new Album("19", "Adele", "Pop", "2008");
+		String[] expectedSongList1 = new String[0];
+		assertArrayEquals(expectedSongList1, album.getSongs(), "Initially, getSongs return an empty array");
+		
+		Album copyAlbum = new Album(album);
+		Song song = new Song("Daydreamer", "Adele", "19");
+		album.addSong(song);
+		String[] expectedSongList2 = new String[1];
+		String expectedSong = "Daydreamer, Adele, 19";
+		expectedSongList2[0] = expectedSong;
+		assertArrayEquals(expectedSongList2, album.getSongs(), "Afterwards, original Album object will have 1 song");
+		assertArrayEquals(expectedSongList1, copyAlbum.getSongs(), "Afterwards, copied Album object will have no song");
 	}
 	
 	@Test

4a7f684bc7a7327fe7b3556cb31c797a0fd2c9c0
BaoNguyen09 <nttbvlogs@gmail.com>
2025-02-23 12:47:28 -0700

    feat: create constructor and getter methods for Playlist class

--- csc_335_music_library/src/model/Playlist.java
+++ csc_335_music_library/src/model/Playlist.java
@@ -1,5 +1,31 @@
 package model;
 
-public class Playlist {
+import java.util.ArrayList;
 
+public class Playlist {
+	private String playlistTitle;
+	private ArrayList<Song> songs;
+	
+	public Playlist(String title) {
+		playlistTitle = title;
+	}
+	
+	public String getPlaylistTitle() {
+		return this.playlistTitle;
+	}
+	
+	public String[] getPlaylistSongs() {
+		int songListLength = songs.size();
+		String[] songList = new String[songListLength];
+		for (int i = 0; i < songListLength; i++) {
+			Song song = songs.get(i);
+			songList[i] = song.toString();
+		}
+		return songList;
+	}
+	
+	@Override
+	public String toString() {
+		return playlistTitle;
+	}
 }

41cfcc8e0b4e10e645d34643ef293b06bbebed14
BaoNguyen09 <nttbvlogs@gmail.com>
2025-02-27 10:44:18 -0700

    feat: set up instance variables and constructor for LibraryModel class

--- csc_335_music_library/src/model/LibraryModel.java
+++ csc_335_music_library/src/model/LibraryModel.java
@@ -1,5 +1,27 @@
 package model;
 
-public class LibraryModel {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class LibraryModel {
+	
+	List<Playlist> playlists;
+	List<Song> favoriteSongs;
+	private Map<String, List<Song>> songByTitle;
+	private Map<String, List<Song>> songByArtist;
+	private Map<String, List<Album>> albumByTitle;
+	private Map<String, List<Album>> albumByArtist;
+	
+	public LibraryModel() {
+		playlists = new ArrayList<Playlist>();
+		favoriteSongs = new ArrayList<Song>();
+		songByTitle = new HashMap<String, List<Song>>();
+		songByArtist = new HashMap<String, List<Song>>();
+		albumByTitle = new HashMap<String, List<Album>>();
+		albumByArtist = new HashMap<String, List<Album>>();
+	}
+	
+	
 }

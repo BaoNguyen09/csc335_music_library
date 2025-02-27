80d2c517e170b725062a50af2995dc7d4fa290b1
BaoNguyen09 <nttbvlogs@gmail.com>
2025-02-27 11:44:44 -0700

    feat: add method to get playlist title and add an instance variable to store playlist by title

--- csc_335_music_library/src/model/LibraryModel.java
+++ csc_335_music_library/src/model/LibraryModel.java
@@ -13,6 +13,7 @@ public class LibraryModel {
 	private Map<String, List<Song>> songByArtist;
 	private Map<String, List<Album>> albumByTitle;
 	private Map<String, List<Album>> albumByArtist;
+	private Map<String, Playlist> playlistByTitle;
 	
 	public LibraryModel() {
 		playlists = new ArrayList<Playlist>();
@@ -21,6 +22,7 @@ public class LibraryModel {
 		songByArtist = new HashMap<String, List<Song>>();
 		albumByTitle = new HashMap<String, List<Album>>();
 		albumByArtist = new HashMap<String, List<Album>>();
+		playlistByTitle = new HashMap<String, Playlist>();
 	}
 	
 	public String[] getSongTitles() {
@@ -54,5 +56,17 @@ public class LibraryModel {
 			i++;
 		}
 		return albumList;
+	}
+	
+	public String[] getPlaylistTitles() {
+		int playlistListLength = playlistByTitle.size();
+		String[] playlistList = new String[playlistListLength];
+		int i = 0;
+		for (String playlistTitle: playlistByTitle.keySet()) {
+			// get playlist title from Playlist object to preserve capitalization
+			playlistList[i] = playlistByTitle.get(playlistTitle).toString();
+			i++;
+		}
+		return playlistList;
 	}
 }

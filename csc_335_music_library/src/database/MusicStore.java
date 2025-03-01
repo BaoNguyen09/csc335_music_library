bcc2cf078e48d2d47cdf7c0a1ca6bb20f6bc8498
Claire Vu <tnv1@arizona.edu>
2025-02-28 21:00:51 -0700

    fix: made MusicStore Jar file compatible by loading textfiles as resources

--- csc_335_music_library/src/database/MusicStore.java
+++ csc_335_music_library/src/database/MusicStore.java
@@ -4,8 +4,9 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
@@ -15,86 +16,80 @@ import model.Album;
 import model.Song;
 
 public class MusicStore {
-	private Map<String, List<Song>> songByTitle;
-	private Map<String, List<Song>> songByArtist;
-	private Map<String, List<Album>> albumByTitle;
-	private Map<String, List<Album>> albumByArtist;
+    private Map<String, List<Song>> songByTitle;
+    private Map<String, List<Song>> songByArtist;
+    private Map<String, List<Album>> albumByTitle;
+    private Map<String, List<Album>> albumByArtist;
 
-	public MusicStore() {
-		songByTitle = new HashMap<String, List<Song>>();
-		songByArtist = new HashMap<String, List<Song>>();
-		albumByTitle = new HashMap<String, List<Album>>();
-		albumByArtist = new HashMap<String, List<Album>>();
-		
-		try {
-			processAlbums();
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to process albums.txt");
-		}
-	}
-	
-	/* 
-	 * @pre songTitle != null
-	 */
-	public List<Song> searchSongByTitle(String songTitle) {
-		List<Song> songsWithTitle = songByTitle.get(songTitle.toUpperCase());
-		return copySongsList(songsWithTitle);
-	}
-	
-	/* 
-	 * @pre artist != null
-	 */
-	public List<Song> searchSongByArtist(String artist) {
-		List<Song> songsWithArtist = songByArtist.get(artist.toUpperCase());
-		return copySongsList(songsWithArtist);
-	}
-	
-	
-	private List<Song> copySongsList(List<Song> songsList){
-		if (songsList == null) {
-			return new ArrayList<Song>();
-		}
-		List<Song> listCopy = new ArrayList<Song>();
-		
-		for (int i= 0; i < songsList.size(); i++) {
-			Song songCopy = new Song(songsList.get(i));
-			listCopy.add(songCopy);
-		}
-		return listCopy;
-	}
-	
-	/* 
-	 * @pre albumTitle != null
-	 */
-	public List<Album> searchAlbumByTitle(String albumTitle) {
-		List<Album> albumsWithTitle = albumByTitle.get(albumTitle.toUpperCase());
-		return copyAlbumsList(albumsWithTitle);
-	}
-	
-	/* 
-	 * @pre artist != null
-	 */
-	public List<Album> searchAlbumByArtist(String artist) {
-		List<Album> albumsWithArtist = albumByArtist.get(artist.toUpperCase());
-		return copyAlbumsList(albumsWithArtist);
-	}
-	
-	
-	private List<Album> copyAlbumsList(List<Album> albumsList){
-		if (albumsList == null) {
-			return new ArrayList<Album>();
-		}
-		
-		List<Album> listCopy = new ArrayList<Album>();
-		for (int i= 0; i < albumsList.size(); i++) {
-			Album albumCopy = new Album(albumsList.get(i));
-			listCopy.add(albumCopy);
-		}
-		return listCopy;
-	}
+    public MusicStore() {
+        songByTitle = new HashMap<>();
+        songByArtist = new HashMap<>();
+        albumByTitle = new HashMap<>();
+        albumByArtist = new HashMap<>();
+        
+        try {
+            processAlbums();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to process albums.txt", e);
+        }
+    }
+    
+    /* 
+     * @pre songTitle != null
+     */
+    public List<Song> searchSongByTitle(String songTitle) {
+        List<Song> songsWithTitle = songByTitle.get(songTitle.toUpperCase());
+        return copySongsList(songsWithTitle);
+    }
+    
+    /* 
+     * @pre artist != null
+     */
+    public List<Song> searchSongByArtist(String artist) {
+        List<Song> songsWithArtist = songByArtist.get(artist.toUpperCase());
+        return copySongsList(songsWithArtist);
+    }
+    
+    private List<Song> copySongsList(List<Song> songsList){
+        if (songsList == null) {
+            return new ArrayList<>();
+        }
+        List<Song> listCopy = new ArrayList<>();
+        for (Song s : songsList) {
+            listCopy.add(new Song(s));
+        }
+        return listCopy;
+    }
+    
+    /* 
+     * @pre albumTitle != null
+     */
+    public List<Album> searchAlbumByTitle(String albumTitle) {
+        List<Album> albumsWithTitle = albumByTitle.get(albumTitle.toUpperCase());
+        return copyAlbumsList(albumsWithTitle);
+    }
+    
+    /* 
+     * @pre artist != null
+     */
+    public List<Album> searchAlbumByArtist(String artist) {
+        List<Album> albumsWithArtist = albumByArtist.get(artist.toUpperCase());
+        return copyAlbumsList(albumsWithArtist);
+    }
+    
+    private List<Album> copyAlbumsList(List<Album> albumsList){
+        if (albumsList == null) {
+            return new ArrayList<>();
+        }
+        
+        List<Album> listCopy = new ArrayList<>();
+        for (Album a : albumsList) {
+            listCopy.add(new Album(a));
+        }
+        return listCopy;
+    }
 
-	
-	/*
+    /*
 	 * Description: Reads and process all albums files listed in the 
 	 *'src/database/albums/fileName' text file. (e.g. processing all 
 	 * <album title>_<artist>.txt files listed in the albums.txt file.
@@ -102,29 +97,29 @@ public class MusicStore {
 	 * @pre each line in fileName is formatted as follows:
 	 * 	<album title>,<artist>
 	 */
-	private void processAlbums() throws IOException  {
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader("src/database/albums/albums.txt"));
-			String line;
-			
-			while ((line = reader.readLine()) != null) {
-				
-				// Each line is formatted <Album Title>,<Artist>
-				String[] temp = line.strip().split(",");
-				String songFileName = temp[0] + "_" + temp[1] + ".txt";
-				String filePath = "src/database/albums/" + songFileName;
-				processFile(filePath);
-				
-			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			throw e;
-		}
-	
-	}
-	
-	/*
+    private void processAlbums() throws IOException {
+        // "albums.txt" is assumed to be in the same package as this class
+        InputStream in = getClass().getResourceAsStream("albums.txt");
+        if (in == null) {
+            throw new FileNotFoundException("Could not find albums.txt");
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Each line: <Album Title>,<Artist>
+                String[] temp = line.strip().split(",");
+                String albumTitle = temp[0];
+                String artist = temp[1];
+
+                String albumResource = albumTitle + "_" + artist + ".txt";
+                // Reading each album individually
+                processFile(albumResource);
+            }
+        }
+    }
+    
+    /*
 	 * @pre 
 	 * The first line albums.txt is formatted as follows:
 	 * 	<album title>,<artist>,<genre>,<year>
@@ -132,51 +127,47 @@ public class MusicStore {
 	 * Each Subsequent line is formatted as follows:
 	 * 	<songTitle>
 	 */
-	private void processFile(String filePath) throws IOException{
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(filePath));
-			
-			// First line is formatted: Album Title, Artist, Genre, Year
-			String line = reader.readLine();
-			String[] albumInfo = line.strip().split(",");
-			
-			String albumTitle = albumInfo[0];
-			String artist = albumInfo[1];
-			Album currAlbum = new Album(albumTitle, artist, albumInfo[2], albumInfo[3]);
-			
-			// For each song in the album, add it to album
-			while ((line = reader.readLine()) != null) {
-				String songTitle = line.strip();
-				Song currSong = new Song(songTitle, artist, albumTitle);
-				currAlbum.addSong(currSong);
-				
-				// Adding to the songs map
-				addToMapList(songByArtist, artist.toUpperCase(), currSong);
-				addToMapList(songByTitle, songTitle.toUpperCase(), currSong);
-				
-			}
-			
-			// Adding to the albums map
-			addToMapList(albumByTitle, albumTitle.toUpperCase(), currAlbum);
-			addToMapList(albumByArtist, artist.toUpperCase(), currAlbum);
-			
-			reader.close();
-			
-		} catch (FileNotFoundException e) {
-	        throw new RuntimeException("Failed to process albums.txt", e);
-		}
-		
-	}
-	
-	// Function adds to the respective map
-	private <K, V> void addToMapList(Map<K, List<V>> map, K key, V value) {
-		
-		// if the key already exist then it just adds the value to the list
-		// else it initializes a list for that key value with that value added
-	    map.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
-	}
-	
-	
+    private void processFile(String resourcePath) throws IOException {
+        InputStream fileIn = getClass().getResourceAsStream(resourcePath);
+        if (fileIn == null) {
+            throw new FileNotFoundException("Could not find " + resourcePath);
+        }
+        
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(fileIn))) {
+            // First line: Album Title, Artist, Genre, Year
+            String line = reader.readLine();
+            if (line == null) {
+                throw new IOException("Empty album file: " + resourcePath);
+            }
+            
+            String[] albumInfo = line.strip().split(",");
+            String albumTitle = albumInfo[0];
+            String artist = albumInfo[1];
+            Album currAlbum = new Album(albumTitle, artist, albumInfo[2], albumInfo[3]);
+            
+            // For each song line, add it to the album
+            while ((line = reader.readLine()) != null) {
+                String songTitle = line.strip();
+                Song currSong = new Song(songTitle, artist, albumTitle);
+                currAlbum.addSong(currSong);
 
+                // Add to the songs map
+                addToMapList(songByArtist, artist.toUpperCase(), currSong);
+                addToMapList(songByTitle, songTitle.toUpperCase(), currSong);
+            }
+            
+            // Add to the albums map
+            addToMapList(albumByTitle, albumTitle.toUpperCase(), currAlbum);
+            addToMapList(albumByArtist, artist.toUpperCase(), currAlbum);
+        }
+    }
+    
+	// Function adds to the respective map
+    private <K, V> void addToMapList(Map<K, List<V>> map, K key, V value) {
+    	
+    	// if the key already exist then it just adds the value to the list
+    	// else it initializes a list for that key value with that value added
+        map.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+    }
 }
+

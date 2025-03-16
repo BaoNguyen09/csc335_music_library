6e06f241d992ef703d5daa843b8292820a12fbc8
BaoNguyen09 <nttbvlogs@gmail.com>
2025-03-16 11:43:46 -0700

    update: use equals method of Song to quickly check duplication

--- csc_335_music_library/src/model/Playlist.java
+++ csc_335_music_library/src/model/Playlist.java
@@ -42,16 +42,8 @@ public class Playlist {
 	}
 	
 	public void addSongToPlaylist(Song song) {
-		// Check if Song instances is duplicated (shallow check)
+		// Check if Song instances is duplicated (deep check)
 		if (!songs.contains(song)) {
-			// Compare song's values with each other (deep check)
-			for (Song songItem: songs) {
-				if (songItem.getSongTitle().equals(song.getSongTitle())
-						&& songItem.getAlbumTitle().equals(song.getAlbumTitle())
-						&& songItem.getArtist().equals(song.getArtist())) {
-					return; // stop if spot duplicated song
-				}
-			}
 			songs.add(new Song(song));
 		}
 	}

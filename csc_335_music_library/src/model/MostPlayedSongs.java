708659d8ee0714149c87590252b5b489bc12e36e
BaoNguyen09 <nttbvlogs@gmail.com>
2025-03-16 15:40:53 -0700

    refactor: remove anti-pattern: temporary field

--- csc_335_music_library/src/model/MostPlayedSongs.java
+++ csc_335_music_library/src/model/MostPlayedSongs.java
@@ -4,15 +4,15 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class MostPlayedSongs extends Playlist{
-	private int lowestStreamCount;
 	
 	public MostPlayedSongs() {
 		super("Most Played Songs Playlist");
-		lowestStreamCount = 0;
 	}
 	
 	@Override
 	public void addSongToPlaylist(Song song) {
+		int lowestStreamCount = songs.get(0).getStreamCount();
+		
 		// Check if Song instances is already added (deep check)
 		if (!songs.contains(song)) {
 			// if stream count of this new song >= lowestStreamCount, add it to the list
@@ -30,7 +30,6 @@ public class MostPlayedSongs extends Playlist{
 		});
 		
 		if (songs.size() > 10) songs.remove(0); // ensure the length of the list is always <= 10
-		lowestStreamCount = songs.get(0).getStreamCount(); // update the lowestStreamCount
 	}
 	
 	/* Disable this method because this playlist is maintained automatically */
